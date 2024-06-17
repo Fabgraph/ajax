77edf6febd4f32ef11064b521ed74a18ffbedd80
Fabrice Domoison <39194172+Fabgraph@users.noreply.github.com>
2024-06-17 16:22:41 +0200

    feat/landing.js

--- src/js/pages/landing.js
+++ src/js/pages/landing.js
@@ -13,3 +13,14 @@ landingMain.innerHTML =
   img('src="../../assets/img/chicken-1618459_1280.jpg"') +
   img('src="../../assets/img/dak-bokkeumtang-749365_1280.jpg"') +
   button("Valider", "submit", "login-button");
+
+
+  fetch('https://chezbobo.onrender.com/products', {
+    method: 'GET'
+  })
+    .then((result) => {
+        return result.json()
+    })
+    .then((javascript) => {
+        console.log('mes données', javascript);
+    })
